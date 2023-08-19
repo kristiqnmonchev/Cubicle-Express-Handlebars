9fd1444be6e2eebffe122b587a9250042178d8ab
Kristiyan Monchev <59092330+kristiqnmonchev@users.noreply.github.com>
2023-08-20 02:52:43 +0300

    init handlebars in project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
+
 const app = express();
 const PORT = 5000;
 
+//Handlebars config
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-    res.send('Express is on')
+    res.render('index')
 })
 
 app.listen(PORT, () => console.log('App is running on port 5000'))
