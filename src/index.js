6a88945cf7a8a420c2d861ab6dde288d43efaff6
Kristiyan Monchev <59092330+kristiqnmonchev@users.noreply.github.com>
2023-08-20 03:57:08 +0300

    add configs files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const handlebars = require('express-handlebars')
+const expressConfig = require('./config/expressConfig')
+const handlebarsConfig = require('./config/handlebarsConfig')
 
 const app = express();
 const PORT = 5000;
+expressConfig(app)
+handlebarsConfig(app)
 
-//Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
-// ==
-
-// Load static Files
-app.use(express.static('./src/static'))
 
 app.get('/', (req, res) => {
     res.render('index')
