f8bb54190c829159889ae165a74f58797a937f37
Kristiyan Monchev <59092330+kristiqnmonchev@users.noreply.github.com>
2023-08-20 16:17:43 +0300

    add filter to cubes

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -11,6 +11,19 @@ exports.create = (cubeData) => {
 
 }
 
-exports.getAll = () => cubes.slice();
+exports.getAll = ( search, from, to) => {
+    
+    let result = cubes.slice();
+    if (search) {
+        result = result.filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+    }
+    if (from) {
+        result = result.filter(x => x.difficultyLevel >= Number(from) )
+    }
+    if (to) {
+        result = result.filter(x => x.difficultyLevel >= Number(to) )
+    }
+    return result
+}
 
 exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId);
