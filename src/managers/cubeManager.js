08772c76cf0640f211e20b8119fab0eb5541e87c
Kristiyan Monchev <59092330+kristiqnmonchev@users.noreply.github.com>
2023-08-23 01:46:10 +0300

    get cubes from DB

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -2,38 +2,26 @@ const uniqId = require('uniqid')
 const mongoose = require('mongoose')
 const cubes = []
 
+// CUBE SCHEMA
 const cubeSchema = new mongoose.Schema({
     name: String,
     description: String,
     imageUrl: String,
     difficultyLevel: Number,
 })
-
 const Cube = mongoose.model('Cube', cubeSchema)
-// module.exports = Cube; 
 
-
-
-// exports.create = (cubeData) => {
-//     const newCube = {
-//         id: uniqId(),
-//         ...cubeData,
-//     }
-//     cubes.push(newCube)
-//     return newCube;
-
-// }
+// ======== CRUD ============ BELOW ============
 
 exports.create = async (cubeData) => {
     const newCube = await Cube.create(cubeData)
-    console.log(newCube)
     return newCube;
 }
 
 
-exports.getAll = ( search, from, to) => {
+exports.getAll = async( search, from, to) => {
     
-    let result = cubes.slice();
+    let result = await Cube.find().lean()
     if (search) {
         result = result.filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
     }
@@ -43,6 +31,7 @@ exports.getAll = ( search, from, to) => {
     if (to) {
         result = result.filter(x => x.difficultyLevel >= Number(to) )
     }
+    // console.log(result)
     return result
 }
 
