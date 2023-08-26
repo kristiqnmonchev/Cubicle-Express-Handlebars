dee5ba01a9fed30c07b85e4e693c87bd346edd42
Kristiyan Monchev <59092330+kristiqnmonchev@users.noreply.github.com>
2023-08-27 02:19:03 +0300

    Login Page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const userModel = require('../models/User')
+const userManager = require('../managers/userManager')
+const cookieParser = require('cookie-parser')
 
 router.get('/register', (req, res) => {
     res.render('registerPage')
@@ -12,10 +14,27 @@ router.post('/register', async (req, res, next) => {
 
     const hash = await bcrypt.hash(password, 5)
     await userModel.create({username, password: hash})
-
+    res.cookie('user', username)
     
-    console.log(hash)
     res.redirect('/')
+})
+
+router.get('/login', async(req, res) => {
+    res.render('loginPage')
+})
+
+router.post('/login', async(req, res) => {
+    const {username, password} = req.body;
+
+    const user = await userManager.login(username);
+    const validate = await bcrypt.compare(password, user.password)
+    
+    if (!validate) {
+        return res.render('404')
+    }
+    res.cookie('user', username)
+    res.redirect('/')
+
 })
 
 
--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,5 @@
 const User = require('../models/User');
 
 exports.create = (username, password) => User.create({username, password})
+
+exports.login = (user) =>  User.findOne({username: user})
